c8468e0d78c0c9a1650c79ad21883b0b0328db64
Rebeca-desen <rebecarss01@gmail.com>
2021-07-15 17:45:50 -0300

    botão no centro

--- loreal/src/App.js
+++ loreal/src/App.js
@@ -6,8 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p className="Text-home">Olá, sejam bem vindos ao futuro. Você está sendo chamado para participar dele antes</p>
-        <button className="next-home" > Quero fazer parte </button>
- 
+       <div className="Button-next-home">
+         <button className="next-home" > Quero fazer parte </button>
+       </div>
+        
       </header>
     </div>
   );

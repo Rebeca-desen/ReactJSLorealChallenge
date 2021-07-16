41962bc4bdbcf80ed635eb933ac19740f5249a68
Rebeca-desen <rebecarss01@gmail.com>
2021-07-16 15:03:05 -0300

    tentativa de redirecionar botão tutorial

--- loreal/src/tutorial.js
+++ loreal/src/tutorial.js
@@ -1,1 +1,12 @@
+import React from 'react'
 import './App.js';
+
+function tutorial (){
+    const buttonTutorial = document.querySelector('.button-tutorial')
+
+    buttonTutorial.addEventListener("click", function(){
+        buttonTutorial.action = "/tutorial"
+        buttonTutorial.method = "GET"
+    })
+
+}
